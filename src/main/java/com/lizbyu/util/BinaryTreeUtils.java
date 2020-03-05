900f31dc8d3a7b16676e4216e3922319807155ba
Uybzil <BigUncleLi@163.com>
2020-03-05 10:46:24 +0800

    feat : add construct bt from pre and in order

--- src/main/java/com/lizbyu/util/BinaryTreeUtils.java
+++ src/main/java/com/lizbyu/util/BinaryTreeUtils.java
@@ -2,6 +2,7 @@ package com.lizbyu.util;
 
 import com.lizbyu.tree.BinaryTree;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BinaryTreeUtils<V> {
@@ -18,6 +19,42 @@ public class BinaryTreeUtils<V> {
         BinaryTree.Node<V> node = new BinaryTree.Node<>(valueSequence.get(index));
         node.setLeft(createRootNode(valueSequence, 2 * index + 1));
         node.setRight(createRootNode(valueSequence, 2 * index + 2));
+        return node;
+    }
+
+    /**
+     * Note : You may assume that duplicates do not exist in the tree.
+     */
+    public BinaryTree<V, BinaryTree.Node<V>> create(V[] preorder, V[]inorder) {
+        return new BinaryTree<>(createRootNode(preorder, inorder));
+    }
+
+    private BinaryTree.Node<V> createRootNode(V[] preorder, V[]inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        V rootVal = preorder[0];
+        int rootIndex = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+        // pre : rootval -- [left] -- [right]
+        // in : [left] -- rootval -- [right]
+        BinaryTree.Node<V> node = new BinaryTree.Node<>(rootVal);
+
+        V[] leftPreorder = Arrays.copyOfRange(preorder, 1, rootIndex + 1);
+        V[] leftInorder = Arrays.copyOfRange(inorder, 0, rootIndex);
+        node.setLeft(createRootNode(leftPreorder, leftInorder));
+
+        V[] rightPreorder = Arrays.copyOfRange(preorder, rootIndex + 1, preorder.length);
+        V[] rightInorder = Arrays.copyOfRange(inorder, rootIndex + 1, inorder.length);
+        node.setRight(createRootNode(rightPreorder, rightInorder));
+
         return node;
     }
 
@@ -39,4 +76,6 @@ public class BinaryTreeUtils<V> {
 
         return isSameTree(p.getLeft(), q.getLeft()) && isSameTree(p.getRight(), q.getRight());
     }
+
+
 }
---
+++ src/test/java/com/lizbyu/util/BinaryTreeUtilsTestCreateFromPI.java
@@ -0,0 +1,59 @@
+package com.lizbyu.util;
+
+import com.lizbyu.tree.BinaryTree;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class BinaryTreeUtilsTestCreateFromPI {
+    private Integer[] preorder;
+    private Integer[] inorder;
+    private List<Integer> treeNode;
+    private static BinaryTreeUtils<Integer> binaryTreeUtils;
+
+    public BinaryTreeUtilsTestCreateFromPI(Integer[] preorder, Integer[] inorder, List<Integer> treeNode) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        this.treeNode = treeNode;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> prepareData() {
+        return Arrays.asList(new Object[][]{
+                {
+                    new Integer[]{1, 2, 3}, new Integer[]{2, 1, 3}, Arrays.asList(1, 2, 3)
+                },
+                {
+                        new Integer[]{3, 9, 20, 15, 7}, new Integer[]{9, 3, 15, 20, 7},
+                        Arrays.asList(3, 9, 20, null, null, 15, 7)
+                },
+                {
+                        new Integer[]{1, 2, 8, 9, 11, 4, 27, 3},
+                        new Integer[]{8, 2, 11, 9, 1, 27, 3, 4},
+                        Arrays.asList(1, 2, 4, 8, 9, 27, null, null, null, 11, null, null, 3)
+                }
+        });
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        System.out.println("setup");
+        binaryTreeUtils = new BinaryTreeUtils<>();
+    }
+
+    @Test
+    public void create() {
+        BinaryTree<Integer, BinaryTree.Node<Integer>> p = binaryTreeUtils.create(preorder, inorder);
+        BinaryTree<Integer, BinaryTree.Node<Integer>> q = binaryTreeUtils.create(treeNode);
+        assertTrue(binaryTreeUtils.isSameTree(p.getRoot(), q.getRoot()));
+    }
+}
